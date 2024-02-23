8f135d8041983e472a754050f588ea480dc33dd5
Asad khan <Asad.khan@moreyeahs.in>
2024-02-23 12:02:46 +0530

    first push

---
+++ src/CustomComponents/CalendarComponents/CustomCalendar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomCalendar = () => {
+  return <div>CustomCalendar</div>;
+};
+
+export default CustomCalendar;
---
+++ src/CustomComponents/NewsComponents/CustomNewsSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomNewsSection = () => {
+  return <div>CustomNewsSection</div>;
+};
+
+export default CustomNewsSection;
